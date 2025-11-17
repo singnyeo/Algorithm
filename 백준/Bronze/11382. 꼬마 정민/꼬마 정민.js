8de944431ff01fd178bb3a64307b1fe75d5d608c
Siyeon Kang <123534119+singnyeo@users.noreply.github.com>
2025-11-17 18:17:22 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().trim().split(' ');
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+const C = Number(input[2]);
+
+const result = A + B + C;
+
+console.log(result);
