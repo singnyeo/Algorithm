a91eb3b75ea532770a59685840757c4f704b183d
Siyeon Kang <123534119+singnyeo@users.noreply.github.com>
2025-11-17 18:04:55 +0900

    [Bronze V] Title: A-B, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().trim().split(' ');
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+const result = A - B;
+
+console.log(result);
